324f0618f662158d0e07f7fb07067d7933e8b8bd
meixsosimp <linhmyx2003@gmail.com>
2024-01-19 17:22:07 +0700

    [LMy]-create_SplashActivity_and_delay_2s

---
+++ app/src/main/java/dev/proptit/messenger/ui/SplashActivity.kt
@@ -0,0 +1,29 @@
+package dev.proptit.messenger.ui
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+import androidx.activity.ComponentActivity
+import dev.proptit.messenger.MainActivity
+import dev.proptit.messenger.R
+
+
+class SplashActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Handler to delay the transition to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+
+}
+
